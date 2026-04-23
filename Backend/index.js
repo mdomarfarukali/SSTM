9dd1a488e57e8752a0af24e4538618ab9a64555d
Md Omar Faruk Ali <149320371+mdomarfarukali@users.noreply.github.com>
2026-04-23 19:50:58 +0530

    Fix CORS allowed origins by removing trailing slash

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: (origin, callback) => {
-    const allowedOrigins = ['https://diva-ten-ebon.vercel.app/', process.env.FRONTEND_URL ];
+    const allowedOrigins = ['https://diva-ten-ebon.vercel.app', process.env.FRONTEND_URL ];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
